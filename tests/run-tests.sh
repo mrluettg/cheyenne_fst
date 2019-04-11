78ffb9a86b77c48426dee838881157aded417277
Francis Tyers <ftyers@prompsit.com>
2019-04-11 22:39:39 +0100

    update test

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,1 +1,5 @@
-cat phon.tsv  | sed 's/\t\t*/\t/g' | cut -f4 | hfst-pair-test ../chy.twol.hfst
+for i in `cat phon.tsv  | sed 's/\t\t*/\t/g' | cut -f4 | sed 's/ /_/g'`; do 
+	x=`echo $i | tr '_' ' '`;
+	res=`echo $x | hfst-pair-test ../chy.twol.hfst`;
+	echo $res": "$x
+done
