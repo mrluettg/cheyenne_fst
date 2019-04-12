c9206543e2c898e0c6777e7d35554d07663f9aef
Matthew Luettgen <mrluettg@iu.edu>
2019-04-12 06:05:18 +0200

    names and twol stuff

--- scripts/sg_pl_to_lexc.py
+++ scripts/sg_pl_to_lexc.py
@@ -20,20 +20,25 @@ for line in file:
 	if singular == "ni." or singular == "na.": continue #common error. 
 	#clean
 	singular = singular.lower()
+	plural = plural.lower()
 	plural = re.sub("ma-", "%{v%}", plural)
 	plural = re.sub("mȧ-", "%{v%}", plural) #weird phonetic thing. Some can't stand on their own and take weird possesives. 
 	plural = re.sub("-", "", plural)
 	plural = re.sub("1", "", plural)
 	plural = re.sub("2", "", plural)
 	plural = re.sub("\.", "", plural)
+	plural = re.sub("ā", "a", plural)
+	plural = re.sub("ē", "e", plural)
+	plural = re.sub("ō", "o", plural)
 	singular = re.sub("ma-", "", singular)
 	singular = re.sub("mȧ-", "", singular)
 	singular = re.sub("-", "", singular)
 	singular = re.sub("1", "", singular)
 	singular = re.sub("2", "", singular)
 	singular = re.sub("\.", "", singular)
-	plural = plural.lower()
-	singular = singular.lower()
+	singular = re.sub("ā", "a", singular)
+	singular = re.sub("ē", "e", singular)
+	singular = re.sub("ō", "o", singular)
 	abstract = ""
 	pos = ""
 	if plural == "o'o": #for some reason many plural are listed as o'o
@@ -43,8 +48,16 @@ for line in file:
 		abstract = get_abstract(plural)
 		pos = get_pos(plural)
 	#print("abstract: " + abstract + " pos: " + pos)
-	if abstract == "UNKNOWN" or pos == "UNKNOWN": continue
-	else: print(singular + ":" + abstract + " " + pos + " ;")
+	if abstract == "UNKNOWN" or pos == "UNKNOWN" or abstract == "" or pos == "": continue
+	e_cons = ["v", "p", "x", "k"]
+	if abstract[-1:] in e_cons or (abstract[-1:] == "m" and singular[-2:-1] == "m"):
+		abstract += "%{e%}"
+	if abstract[-1:] == "t": 
+		abstract += "%{s%}%{e%}"
+	if singular[-2:] == "\'o": 
+		abstract += "%{o%}"
+	print(singular + ":" + abstract + " " + pos + " ;")
+	
  
 	
 	
